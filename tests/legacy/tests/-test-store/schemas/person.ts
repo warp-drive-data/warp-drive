03c040101ac95ccde7448c896ef5e12369da4203
Rich Glazerman <rglazerman@squareup.com>
2025-10-29 14:27:47 -0400

    handle no primitive type passed in

--- tests/legacy/tests/-test-store/schemas/person.ts
+++ tests/legacy/tests/-test-store/schemas/person.ts
@@ -19,7 +19,7 @@ export const PersonSchema = withLegacy({
     withFragmentDefaults('name'),
     withFragmentArrayDefaults('names'),
     withFragmentArrayDefaults('addresses'),
-    withArrayDefaults('string', 'titles'),
+    withArrayDefaults('titles', 'string'),
   ],
 });
 
--- warp-drive-packages/legacy/src/model-fragments/utilities/with-array-defaults.ts
+++ warp-drive-packages/legacy/src/model-fragments/utilities/with-array-defaults.ts
@@ -2,25 +2,40 @@
  * Used as a helper to setup the relevant parts of an array
  * schema and add extensions etc.
  *
- * @param primitiveType The primitive type of items in the array
  * @param arrayName The name of the array
+ * @param primitiveType The primitive type of items in the array (optional)
  * @returns The schema for an array
  */
-export function withArrayDefaults<PrimitiveType extends string, ArrayName extends string>(
-  primitiveType: PrimitiveType,
-  arrayName: ArrayName
-): {
-  kind: 'array';
-  name: ArrayName;
-  type: `array:${PrimitiveType}`;
-  options: {
-    arrayExtensions: string[];
-  };
-} {
+export function withArrayDefaults<ArrayName extends string, PrimitiveType extends string>(
+  arrayName: ArrayName,
+  primitiveType?: PrimitiveType
+): PrimitiveType extends undefined
+  ? {
+      kind: 'array';
+      name: ArrayName;
+      type: 'array';
+      options: {
+        arrayExtensions: string[];
+      };
+    }
+  : {
+      kind: 'array';
+      name: ArrayName;
+      type: `array:${PrimitiveType}`;
+      options: {
+        arrayExtensions: string[];
+      };
+    };
+
+export function withArrayDefaults<ArrayName extends string, PrimitiveType extends string>(
+  arrayName: ArrayName,
+  primitiveType?: PrimitiveType
+) {
+  const type = primitiveType ? (`array:${primitiveType}` as const) : ('array' as const);
   return {
     kind: 'array' as const,
     name: arrayName,
-    type: `array:${primitiveType}` as const,
+    type,
     options: {
       arrayExtensions: ['ember-object', 'ember-array-like', 'fragment-array'],
     },
