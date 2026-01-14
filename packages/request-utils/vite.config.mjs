3b7060d45117b5b4801faedd057e29d733c4ffe1
Arthur de Souza <4565050+arthurrsouza@users.noreply.github.com>
2026-01-14 11:11:41 -0300

    feat: wire up file exports

--- packages/request-utils/vite.config.mjs
+++ packages/request-utils/vite.config.mjs
@@ -1,7 +1,13 @@
 import { createConfig } from '@warp-drive/internal-config/vite/config.js';
 
 export const externals = ['@ember/debug', 'ember-inflector'];
-export const entryPoints = ['src/index.ts', 'src/string.ts', 'src/deprecation-support.ts', 'src/handlers.ts'];
+export const entryPoints = [
+  'src/index.ts',
+  'src/string.ts',
+  'src/deprecation-support.ts',
+  'src/handlers.ts',
+  'src/graphql.ts',
+];
 
 export default createConfig(
   {
