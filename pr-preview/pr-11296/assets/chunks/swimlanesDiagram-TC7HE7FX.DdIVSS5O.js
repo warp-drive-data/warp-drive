c20962011bbceef66359aed07c6273bb01834900
gitKrystan <gitKrystan@users.noreply.github.com>
2026-09-26 10:58:22 +0000

    Remove preview for PR 11296 🛬

--- pr-preview/pr-11296/assets/chunks/swimlanesDiagram-TC7HE7FX.DdIVSS5O.js
+++
@@ -1,8 +0,0 @@
-import"./src.D83aMQU4.js";import{n as e}from"./chunk-Y2CYZVJY.DsF7k-Jl.js";import"./chunk-O7XYJQB3.L82K3GrQ.js";import"./chunk-ZIGJFQKS.1BsKyOjH.js";import"./chunk-7PRAP22T.ClTSLsk5.js";import"./chunk-742MDFTN.Coo-bq_H.js";import"./chunk-MBY4JIJT.DIaFAdMu.js";import"./chunk-7INBJB4K.C-T7xRGb.js";import"./chunk-5DYCD2WN.oVesBTmb.js";import"../app.DdOBh1XV.js";import"./chunk-GWA4HPMP.faYGKYQC.js";import"./chunk-XXDRQBXY.CNSuccmt.js";import"./chunk-WEXAMYUT.B_r3kYHH.js";import{r as t,t as n}from"./chunk-7M6MHVWA.lMw6sgye.js";var r=n({styles:e(e=>`${t(e)}
-  .swimlane.cluster:not([data-color-id]) rect {
-    stroke: ${e.clusterBorder} !important;
-  }
-  [data-look="neo"].cluster rect {
-    filter: none;
-  }
-`,`getStyles`)});export{r as diagram};
